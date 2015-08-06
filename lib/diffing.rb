a8633e48443dd8f28e6d892c5f6e62d2d79c7e3d
4urbanoff <4urbanoff@gmail.com>
2015-08-06 18:30:55 +0300

    speed test

--- lib/diffing.rb
+++ lib/diffing.rb
@@ -1,7 +1,7 @@
-require 'diffing/diff'
-require 'diffing/part'
-require 'diffing/formats/ascii'
-require 'diffing/formats/html'
+require_relative 'diffing/diff'
+require_relative 'diffing/part'
+require_relative 'diffing/formats/ascii'
+require_relative 'diffing/formats/html'
 
 
 module Diffing
@@ -25,3 +25,113 @@ module Diffing
   end
 
 end
+
+
+from = "To create the longest common subsequence from a collection of k-candidates, a grid with each file's contents on each axis is crefsdated. The k-candidates are marked on the grid. A common subsequence can be created by joining marked coordinates of the grid such that any increase in i is accompanied by an increase in j.
+This is illustrated in the diagram to the right.
+Black dots represent candidates that would have to be considered by tdashe simple algorithm and the black lines are connections that create common subsequences of length 3.
+Red dots represent k-candidates that are considered by the Hunt–McIlroy algorithm and the red line is the connection that creates a common subsequence of length 3.Computer science is the scientific and practical approach to computation and its applications. It is the systematic study of the feasibility, structure, expression, and mechanization of the methodical procedures (or algorithms) that underlie the acquisition, representation, processing, storage, communication of, and access to information, whether such informati423on is encoded as bits in a computer memory or transcribed in genes and protein structures in a biological cell.[1] An alternate, more succinct definition of computer science is the study of automating algorithmic processes that scale. A computer scientist specializes in the theory of computation and the design of computational systems.[2]
+
+Its subfields can be divided into a variety of theoretical and practical di43sciplines. Some fields, such as computational complexity theory (which explores the fundamental properties of computational and intractable problems), are highly abstract, while fields such as computer graphics emphasize real-world visual applications. Still other fields focus on the challenges in implementing computation. For example, programming language theorcdsay considers various approaches to the description of computation, while the study of computer programming itself investigates various aspects of the234 use of programming language and complex systems. Human–computer interaction considers the challenges in making computers and 423computations useful, usable, and universally accessible to humans.During the 1940s, as new and more powerful computing machines were developed, the term computer came to refer to the machines rather than their human predecessors.[10] As it became clear that computers could be used for more than just mathematical calculations, the field of computer science broadened to study computation in general. Computer science began to be established as a distinct academic discipline in the 1950s and early 1960s.[11][12] The world's first computer science degree program, the Cambridge Diploma in Computer Science, began at the University of Cambridge Computer Laboratory in 1953. The first computer science degree program in the United States was formed at Purdue University in 1962.[13] Since practical computers became available, many applications of computing have become distinct areas of study in their own rights.
+
+Although many initially 54235believed it was impossible that computers themselves could actually be a scientific field of study, in the late fifties it gradually became accepted among the greater academic population.[14][15] It is the now well-known IBM brand that formed part of the computer science revolution during this time. IBM (short ffsdor International Business Machines) released the IBM 704[16] and later the IBM 709[17] computers, which werfsde widely used during the exploration period of such devices. Still, working with the IBM [computer] was frustrating ... if you had misplaced as much as one letter in one instruction, the program would crash, and you would have to start the whole process over again.[14] During the late 1950s, the computer science discipline was very much in its developmental stages, and such issues were commonplace"
+
+
+to   = "To create the longest common subsequencedas from a collection of k-candidates, a grid with each file's contents on each axis is created. The k-candidates are marked on the grid. A common subsequence can be created by joining marked coordinates of the grid such that any increase in i is accompanied by an increase in j.
+This is illustrated in the diagram to the right.
+Black dots represent candiczxdates that would have to be dsconsidered by the simple algorithm and the black lines are connections that create common subsequences of length 3.
+Red dots represent k-candidates that are considered by the Hunt–McIlroy algorithm and the red line is the connection that creates a common subsequence of length 3.Computer science is the scientific and practical approach to computation and its applications. It is the systematic study of the feasibility, structure, expression, and mechanization of the methodical procedures (or algorithms) that underlie the acquisition, representation, processing, storage, communication of, and access to information, whether such information is encoded as bits in a computer memory or transcribed in genes afsdd protein structures in a biological cell.[1] An alternate, more succinct definition of computer science is the study of autofsdmating algorithmic processes that scale. A computer scientist specializes in the theory of computation and the design of computational systems.[2]
+
+Its subfields can be divided into a variety of theoretical and practical disciplines. Some fields, such as computational complexity theory (which explores the fundamental properties of computational and intractable problems), are highly abstract, while fields such as computer graphics emphasize real-world visual applications. Still other fields focus on the challenges in implementing computation. For example, programming language theory conscfsdiders vdasarious approaches to the description of computation, while the study of computer progfsdramming itself investigates various aspeczxcts of the use of programming language and complex systems. Human–computer interaction considers the challenges in making computers and computations useful, usable, and universally accessiblfsde to humans.During the 1940s, as new and more powerful computing machines were developed, the term computer came to refer to the machines rather than their human predecessors.[10] As it became clear that computers could be used for more than just mathematical calculations, the field of computer science broadened to study computation in general. Computer science began to be established as a distinct academic discipline in the 1950s and early 1960s.[11][12] The world's first computer science degree program, the Cambridge Diploma in Computer Science, began at the University of Cambridge Computer Laboratory in 1953. The first computer science degree program in the United States was formed at Purdue University in 1962.[13] Since practical computers became available, many applications of computing have become distinct areas of study in their own rights.
+
+Although many initially believed it was impossible that computers themselves could actually be a scientific field of study, in the late fifties it gradually became accepted among the greater academic population.[14][15] It is the now well-known IBM brand tfsdhat formed part of the computer science revolution during this time. IBM (short for International Business Machines) released the IBM 704[16] and later the IBM 709[534517] computers, which were widely used during the exploration period of such devices. Still, working with the IBM [computer] was frustrating ... if you had misplaced as much as one letter in one instruction, the program would crash, and you would have to start the whole process over again.[14] During the late 1950s, the computer science discipline was very much in its developmental stages, and such issues were commonplace"
+
+
+
+#from = "To create the longest common subsequence from a collection of k-candidates, a grid with each file's contents on each axis is crefsdated. The k-candidates are marked on the grid. A common subsequence can be created by joining marked coordinates of the grid such that any increase in i is accompanied by an increase in j.
+#This is illustrated in the diagram to the right.
+#Black dots represent candidates that would have to be considered by tdashe simple algorithm and the black lines are connections that create common subsequences of length 3.
+#Red dots represent k-candidates that are considered by the Hunt–McIlroy algorithm and the red line is the connection that creates a common subsequence of length 3.Computer science is the scientific and practical approach to computation and its applications. It is the systematic study of the feasibility, structure, expression, and mechanization of the methodical procedures (or algorithms) that underlie the acquisition, representation, processing, storage, communication of, and access to information, whether such informati423on is encoded as bits in a computer memory or transcribed in genes and protein structures in a biological cell.[1] An alternate, more succinct definition of computer science is the study of automating algorithmic processes that scale. A computer scientist specializes in the theory of computation and the design of computational systems.[2]
+#
+#Its subfields can be divided into a variety of theoretical and practical di43sciplines. Some fields, such as computational complexity theory (which explores the fundamental properties of computational and intractable problems), are highly abstract, while fields such as computer graphics emphasize real-world visual applications. Still other fields focus on the challenges in implementing computation. For example, programming language theorcdsay considers various approaches to the description of computation, while the study of computer programming itself investigates various aspects of the234 use of programming language and complex systems. Human–computer interaction considers the challenges in making computers and 423computations useful, usable, and universally accessible to humans.During the 1940s, as new and more powerful computing machines were developed, the term computer came to refer to the machines rather than their human predecessors.[10] As it became clear that computers could be used for more than just mathematical calculations, the field of computer science broadened to study computation in general. Computer science began to be established as a distinct academic discipline in the 1950s and early 1960s.[11][12] The world's first computer science degree program, the Cambridge Diploma in Computer Science, began at the University of Cambridge Computer Laboratory in 1953. The first computer science degree program in the United States was formed at Purdue University in 1962.[13] Since practical computers became available, many applications of computing have become distinct areas of study in their own rights."
+#
+#
+#to   = "To create the longest common subsequencedas from a collection of k-candidates, a grid with each file's contents on each axis is created. The k-candidates are marked on the grid. A common subsequence can be created by joining marked coordinates of the grid such that any increase in i is accompanied by an increase in j.
+#This is illustrated in the diagram to the right.
+#Black dots represent candiczxdates that would have to be dsconsidered by the simple algorithm and the black lines are connections that create common subsequences of length 3.
+#Red dots represent k-candidates that are considered by the Hunt–McIlroy algorithm and the red line is the connection that creates a common subsequence of length 3.Computer science is the scientific and practical approach to computation and its applications. It is the systematic study of the feasibility, structure, expression, and mechanization of the methodical procedures (or algorithms) that underlie the acquisition, representation, processing, storage, communication of, and access to information, whether such information is encoded as bits in a computer memory or transcribed in genes afsdd protein structures in a biological cell.[1] An alternate, more succinct definition of computer science is the study of autofsdmating algorithmic processes that scale. A computer scientist specializes in the theory of computation and the design of computational systems.[2]
+#
+#Its subfields can be divided into a variety of theoretical and practical disciplines. Some fields, such as computational complexity theory (which explores the fundamental properties of computational and intractable problems), are highly abstract, while fields such as computer graphics emphasize real-world visual applications. Still other fields focus on the challenges in implementing computation. For example, programming language theory conscfsdiders vdasarious approaches to the description of computation, while the study of computer progfsdramming itself investigates various aspeczxcts of the use of programming language and complex systems. Human–computer interaction considers the challenges in making computers and computations useful, usable, and universally accessiblfsde to humans.During the 1940s, as new and more powerful computing machines were developed, the term computer came to refer to the machines rather than their human predecessors.[10] As it became clear that computers could be used for more than just mathematical calculations, the field of computer science broadened to study computation in general. Computer science began to be established as a distinct academic discipline in the 1950s and early 1960s.[11][12] The world's first computer science degree program, the Cambridge Diploma in Computer Science, began at the University of Cambridge Computer Laboratory in 1953. The first computer science degree program in the United States was formed at Purdue University in 1962.[13] Since practical computers became available, many applications of computing have become distinct areas of study in their own rights."
+
+
+
+#from = "To create the longest common subsequence from a collection of k-candidates, a grid with each file's contents on each axis is crefsdated. The k-candidates are marked on the grid. A common subsequence can be created by joining marked "
+#
+#
+#to   = "To create the longest common subsequencedas from a collection of k-candidates, a grid with each file's contents on each axis is created. The k-candidates are marked on the grid. A common subsequence can be created by joining marked"
+
+#from = "To create the longest common subsequence from a collection of k-candidates, a grid with each file's contents on each axis is crefsdated. The k-candidates are marked on the grid. A common subsequence can be created by joining marked coordinates of the grid such that any increase in i is accompanied by an increase in j.
+#This is illustrated in the diagram to the right.
+#Black dots represent candidates that would have to be considered by tdashe simple algorithm and the black lines are connections that create common subsequences of length 3.
+#Red dots represent k-candidates that are considered by the Hunt–McIlroy algorithm and the red line is the connection that creates a common subsequence of length 3.Computer science is the scientific and practical approach to computation and its applications. It is the systematic study of the feasibility, structure, expression, and mechanization of the methodical procedures (or algorithms) that underlie the acquisition, representation, processing, storage, communication of, and access to information, whether such informati423on is encoded as bits in a computer memory or transcribed in genes and protein structures in a biological cell.[1] An alternate, more succinct definition of computer science is the study of automating algorithmic processes that scale. A computer scientist specializes in the theory of computation and the design of computational systems.[2]"
+#
+#
+#to   = "To create the longest common subsequencedas from a collection of k-candidates, a grid with each file's contents on each axis is created. The k-candidates are marked on the grid. A common subsequence can be created by joining marked coordinates of the grid such that any increase in i is accompanied by an increase in j.
+#This is illustrated in the diagram to the right.
+#Black dots represent candiczxdates that would have to be dsconsidered by the simple algorithm and the black lines are connections that create common subsequences of length 3.
+#Red dots represent k-candidates that are considered by the Hunt–McIlroy algorithm and the red line is the connection that creates a common subsequence of length 3.Computer science is the scientific and practical approach to computation and its applications. It is the systematic study of the feasibility, structure, expression, and mechanization of the methodical procedures (or algorithms) that underlie the acquisition, representation, processing, storage, communication of, and access to information, whether such information is encoded as bits in a computer memory or transcribed in genes afsdd protein structures in a biological cell.[1] An alternate, more succinct definition of computer science is the study of autofsdmating algorithmic processes that scale. A computer scientist specializes in the theory of computation and the design of computational systems.[2]"
+
+
+
+
+#t= Time.now
+#puts diff = Diffing.by_words( from, to )
+#p Time.now - t
+
+$tt = 0
+
+
+r = 0
+diff  = nil
+1.times do
+  t= Time.now
+  diff = Diffing.by_words( from, to )
+  p tt = Time.now - t
+  r += tt
+end
+
+p "~ #{ r/10.to_f }"
+p $tt
+
+
+puts diff
+
+
+
+#
+#require 'diff-lcs'
+#
+#
+#seq1 = from.split(' ' )
+#seq2 = to.split(' ')
+#
+#
+#
+#t= Time.now
+#
+#diff = Diff::LCS.diff(seq1, seq2)
+#
+#p Time.now - t
+#
+#
+#diff.flatten.each do |change|
+#
+#  action   = change.instance_variable_get( :@action )
+#  position = change.instance_variable_get( :@position )
+#  element  = change.instance_variable_get( :@element )
+#
+#  p "#{action} #{position} #{element}"
+#
+#  #changes.last
+#
+#end
--- lib/diffing/diff.rb
+++ lib/diffing/diff.rb
@@ -10,13 +10,13 @@ module Diffing
       @parts   = calculate( split( from.to_s ), split( to.to_s ) ).flatten
     end
 
-    def format( format )
+    def format( formatter )
       result = []
       @parts.each do |part|
-        result << format.source( part.source )              if part.source?
-        result << format.insert( part.insert )              if part.insert? && !part.remove?
-        result << format.remove( part.remove )              if part.remove? && !part.insert?
-        result << format.change( part.remove, part.insert ) if part.insert? && part.remove?
+        result << formatter.source( part.source )              if part.source?
+        result << formatter.insert( part.insert )              if part.insert? && !part.remove?
+        result << formatter.remove( part.remove )              if part.remove? && !part.insert?
+        result << formatter.change( part.remove, part.insert ) if part.insert? && part.remove?
       end
       result.join
     end
@@ -46,7 +46,7 @@ module Diffing
 
     def find_middle( from, to, min = 0, max = nil )
 
-      return nil if from.empty? or to.empty?
+      return nil if from.empty? || to.empty?
 
       max  = from.size unless max
       size = min + ( ( max - min ) / 2.to_f ).round
@@ -56,7 +56,7 @@ module Diffing
         if found = find_middle_index( to, subset )
 
           return (
-            size != min and find_middle( from, to, size, max ) or
+            size != min && find_middle( from, to, size, max ) ||
             (
               from_l = from[ 0...first ]
               to_l   = to[ 0...found ]
@@ -70,13 +70,186 @@ module Diffing
 
       end
 
-      size != max and find_middle( from, to, min, size ) or nil
+      size != max && find_middle( from, to, min, size ) || nil
 
+    end
+
+    def scan_index( set, search_subset )
+
+      offset = 0
+      while found = set.slice( offset, set.size ).index( search_subset ) do
+        yield found
+        offset += found + 1
+      end
+
+    end
+
+    def time
+      t= Time.now
+      r = yield
+      $tt += Time.now - t
+      r
     end
 
     def find_middle_index( set, search_subset )
       return set.index( search_subset ) if set.is_a?( String ) && search_subset.is_a?( String )
-      subsets_each( set, search_subset.size ){ |subset, first, last| return first if subset == search_subset }
+
+#      $tt ||= 0
+#      t= Time.now
+#      set_join = set.join( '|' )
+#      $tt += Time.now - t
+#
+#      if found = set_join.index( search_subset.join( '|' ) + '|' )
+#        size = 0
+#        set.each_with_index { |subset, index| return index if size == found; size += subset.size + 1 }
+#      end
+#
+
+#      offset = 0
+#      while found = set[ offset, set.size ].index( search_subset[0] ) do
+#        return found if set[ found, search_subset.size ] == search_subset
+#        offset += found + 1
+#      end
+
+
+#      set.each_with_index do |subset, index|
+#
+#        t= Time.now
+#        sl = set[ index, search_subset.size ]
+#        $tt += Time.now - t
+#
+#        return index if  sl == search_subset
+#
+#      end
+
+#      set = set.dup
+#
+#      index = 0
+#      while time{ set.shift }
+#        return index if set[ 0, search_subset.size ] == search_subset
+#        index += 1
+#      end
+#
+
+#
+
+      ## FASTEST
+
+      set = set.dup
+
+      #t= Time.now
+      set_join = set.join
+      search_subset_join = search_subset.join
+      #$tt += Time.now - t
+      #offset = 0
+      #with_offset = 0
+        index = 0
+
+
+      scan_index( set_join, search_subset_join ) do |found|
+
+
+
+        size = 0
+        while size != found
+          #p "found: #{found}, size: #{size}"
+          break if size + set[0].size > found
+          size  += set.shift.size
+          index += 1
+        end
+
+        return index if set.slice(0, search_subset.size ) == search_subset
+      end
+
+      ## FASTEST
+
+#
+#        set[ with_offset, set.size ].each_with_index { |subset, index|
+#
+#            with_offset += index + 1
+#
+#
+#          if size == found && set[ index, search_subset.size] == search_subset
+#
+#
+#            return index
+#
+#
+#          end
+#
+#
+#        size += subset.size
+#
+#        }
+#
+#        offset += found + 1
+#      end
+
+
+#      set_join.scan( search_subset_join ) do
+#        found = $~.offset(0)[0]
+#
+#        size = 0
+#
+#        set[ with_offset, set.size ].each_with_index { |subset, index|
+#
+#            with_offset += index + 1
+#
+#
+#          if size == found && set[ index, search_subset.size] == search_subset
+#
+#
+#            return index
+#
+#
+#          end
+#
+#
+#        size += subset.size
+#
+#        }
+#
+#      end
+
+
+#      while found = set_join[ offset, set_join.size ].index( search_subset_join )
+#
+#        while found = set[ offset, set.size ].index( search_subset[0] ) do
+#          return found if set[ found, search_subset.size ] == search_subset
+#          offset += found + 1
+#        end
+#
+#
+#
+#        size = 0
+#
+#        set[ with_offset, set.size ].each_with_index { |subset, index|
+#
+#            with_offset += index + 1
+#
+#
+#          if size == found && set[ index, search_subset.size] == search_subset
+#
+#
+#            return index
+#
+#
+#          end
+#
+#
+#        size += subset.size
+#
+#        }
+#
+#        offset += found + 1
+#
+#      end
+
+#      subsets_each( set, search_subset.size ){ |subset, first, last| return first if subset.eql?( search_subset )}
+#      if found = set.join.index( search_subset.join )
+#        size = 0
+#        set.each_with_index { |subset, index| return index if size == found; size += subset.size }
+#      end
       nil
     end
 
