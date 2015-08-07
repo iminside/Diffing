427c00098084bbc5011abbd5e6e48c4f00a49cdd
4urbanoff <4urbanoff@gmail.com>
2015-08-07 10:01:18 +0300

    speed update

--- lib/diffing/diff.rb
+++ lib/diffing/diff.rb
@@ -77,82 +77,24 @@ module Diffing
     def scan_index( set, search_subset )
 
       offset = 0
-      while found = set.slice( offset, set.size ).index( search_subset ) do
+      while found = set.slice( offset..-1 ).index( search_subset ) do
         yield found
         offset += found + 1
       end
 
     end
 
-    def time
-      t= Time.now
-      r = yield
-      $tt += Time.now - t
-      r
-    end
-
     def find_middle_index( set, search_subset )
       return set.index( search_subset ) if set.is_a?( String ) && search_subset.is_a?( String )
 
-#      $tt ||= 0
-#      t= Time.now
-#      set_join = set.join( '|' )
-#      $tt += Time.now - t
-#
-#      if found = set_join.index( search_subset.join( '|' ) + '|' )
-#        size = 0
-#        set.each_with_index { |subset, index| return index if size == found; size += subset.size + 1 }
-#      end
-#
-
-#      offset = 0
-#      while found = set[ offset, set.size ].index( search_subset[0] ) do
-#        return found if set[ found, search_subset.size ] == search_subset
-#        offset += found + 1
-#      end
-
-
-#      set.each_with_index do |subset, index|
-#
-#        t= Time.now
-#        sl = set[ index, search_subset.size ]
-#        $tt += Time.now - t
-#
-#        return index if  sl == search_subset
-#
-#      end
-
-#      set = set.dup
-#
-#      index = 0
-#      while time{ set.shift }
-#        return index if set[ 0, search_subset.size ] == search_subset
-#        index += 1
-#      end
-#
-
-#
-
-      ## FASTEST
-
       set = set.dup
 
-      #t= Time.now
-      set_join = set.join
-      search_subset_join = search_subset.join
-      #$tt += Time.now - t
-      #offset = 0
-      #with_offset = 0
-        index = 0
+      index = 0
 
-
-      scan_index( set_join, search_subset_join ) do |found|
-
-
+      scan_index( set.join, search_subset.join ) do |found|
 
         size = 0
         while size != found
-          #p "found: #{found}, size: #{size}"
           break if size + set[0].size > found
           size  += set.shift.size
           index += 1
@@ -160,96 +102,6 @@ module Diffing
 
         return index if set.slice(0, search_subset.size ) == search_subset
       end
-
-      ## FASTEST
-
-#
-#        set[ with_offset, set.size ].each_with_index { |subset, index|
-#
-#            with_offset += index + 1
-#
-#
-#          if size == found && set[ index, search_subset.size] == search_subset
-#
-#
-#            return index
-#
-#
-#          end
-#
-#
-#        size += subset.size
-#
-#        }
-#
-#        offset += found + 1
-#      end
-
-
-#      set_join.scan( search_subset_join ) do
-#        found = $~.offset(0)[0]
-#
-#        size = 0
-#
-#        set[ with_offset, set.size ].each_with_index { |subset, index|
-#
-#            with_offset += index + 1
-#
-#
-#          if size == found && set[ index, search_subset.size] == search_subset
-#
-#
-#            return index
-#
-#
-#          end
-#
-#
-#        size += subset.size
-#
-#        }
-#
-#      end
-
-
-#      while found = set_join[ offset, set_join.size ].index( search_subset_join )
-#
-#        while found = set[ offset, set.size ].index( search_subset[0] ) do
-#          return found if set[ found, search_subset.size ] == search_subset
-#          offset += found + 1
-#        end
-#
-#
-#
-#        size = 0
-#
-#        set[ with_offset, set.size ].each_with_index { |subset, index|
-#
-#            with_offset += index + 1
-#
-#
-#          if size == found && set[ index, search_subset.size] == search_subset
-#
-#
-#            return index
-#
-#
-#          end
-#
-#
-#        size += subset.size
-#
-#        }
-#
-#        offset += found + 1
-#
-#      end
-
-#      subsets_each( set, search_subset.size ){ |subset, first, last| return first if subset.eql?( search_subset )}
-#      if found = set.join.index( search_subset.join )
-#        size = 0
-#        set.each_with_index { |subset, index| return index if size == found; size += subset.size }
-#      end
       nil
     end
 
